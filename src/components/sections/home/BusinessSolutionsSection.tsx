19c7ea4400a55e457e1e1981b20f95cf9e9677a7
syedrizwan11 <rsg.sr27@gmail.com>
2025-08-09 18:11:38 +0500

    business solutions with image carousal section

---
+++ src/components/sections/home/BusinessSolutionsSection.tsx
@@ -0,0 +1,13 @@
+import { ImageCarousalWithTabs } from "@/components/ImageCarousalWithTabs"
+
+export const BusinessSolutionsSection = () => {
+  return (
+    <div className="bg-backgroundGradient bg-no-repeat bg-[center_top] bg-blueDeepDark min-h-screen  px-[5%] py-20">
+      <div className="text-heading pb-4 text-white text-center">
+        Everything You Need To <br /> Run And Grow Your Business
+      </div>
+
+      <ImageCarousalWithTabs />
+    </div>
+  )
+}
